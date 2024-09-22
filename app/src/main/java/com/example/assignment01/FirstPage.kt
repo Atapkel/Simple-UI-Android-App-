9c815bb8254bf89dd3861289ec6294e209c6c8b0
Atapkel Beibarys <beibarysatapkel@gmail.com>
2024-09-22 17:09:22 +0500

    Fonts does not work

--- app/src/main/java/com/example/assignment01/FirstPage.kt
+++ app/src/main/java/com/example/assignment01/FirstPage.kt
@@ -2,10 +2,10 @@ package com.example.assignment01
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -29,130 +30,124 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-class FirstPage: ComponentActivity() {
+class FirstPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val backgroundGradient = Brush.linearGradient(
-            colors =
-            listOf(
-                Color(0xFF12AA73),
-                Color(0xCF135B46),
-                Color(0xCF135B46)
-            )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun First() {
+    val backgroundGradient = Brush.linearGradient(
+        colors =
+        listOf(
+            Color(0xFF12AA73),
+            Color(0xCF135B46),
+            Color(0xCF135B46)
         )
-        setContent() {
+    )
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(30.dp))
 
-            Column(
-                modifier = Modifier
-                    .background(backgroundGradient)
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-//                verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+
+
+        Column(
+            modifier = Modifier
+                .background(backgroundGradient)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+
             ) {
-                Spacer(modifier = Modifier.height(calcDp(86.03)))
-                Image(
-                    painter = painterResource(id = R.drawable.smartr),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .width(calcDp(97.6))
-                        .height(calcDp(19.77))
-                )
-                Spacer(modifier = Modifier.height(calcDp(52.98)))
+            Spacer(modifier = Modifier.height(86.03.dp))
+            Image(
+                painter = painterResource(id = R.drawable.smartr),
+                contentDescription = null,
+                modifier = Modifier
+                    .width((97.6).dp)
+                    .height((19.77.dp))
+            )
+            Spacer(modifier = Modifier.height((52.98.dp)))
 
-                Image(
-                    painter = painterResource(id = R.drawable.signup_man),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .width(calcDp(inDpI = 200))
-                        .height(calcDp(inDpI = 211))
-                )
-                Spacer(modifier = Modifier.height(calcDp(19.8)))
+            Image(
+                painter = painterResource(id = R.drawable.signup_man),
+                contentDescription = null,
+                modifier = Modifier
+                    .width((200.dp))
+                    .height((211.dp))
+            )
+            Spacer(modifier = Modifier.height((19.8).dp))
 
+            Text(
+                text = "Fresh look, same login.",
+                color = Color(0xFFF4F4F4),
+                fontFamily = FontFamily(Font(R.font.poppins6)),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.height((34.76.dp)))
+
+            Column(verticalArrangement = Arrangement.SpaceBetween) {
+                CreateTextWithIcon(
+                    text = "SmartRecruiters candidate portal is now Smartr.",
+                    id = R.drawable.resource_switch,
+                    contentDescriptionOfIcon = ""
+                )
+                Spacer(modifier = Modifier.height((34.46.dp)))
+
+                CreateTextWithIcon(
+                    text = "Enter the same login info used for your SmartrProfile",
+                    id = R.drawable.log_in,
+                    contentDescriptionOfIcon = ""
+                )
+                Spacer(modifier = Modifier.height((34.46.dp)))
+
+                CreateTextWithIcon(
+                    text = "If login details were saved, you may need to re-save.",
+                    id = R.drawable.refresh,
+                    contentDescriptionOfIcon = ""
+                )
+
+            }
+            Spacer(modifier = Modifier.height((48.43.dp)))
+
+            Text(
+                text = "Why Smartr? Read here",
+                fontFamily = FontFamily(Font(R.font.poppins6)),
+                fontSize = 10.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color(0xFFF4F4F4),
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height((8.99).dp))
+
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFBFDBD1),
+                    contentColor = Color(0xCC135B46)
+                ),
+                shape = RoundedCornerShape(30.dp),
+                modifier = Modifier
+                    .width(272.dp)
+                    .height(42.dp)
+            ) {
                 Text(
-                    text = "Fresh look, same login.",
-                    color = Color(0xFFF4F4F4),
-                    fontFamily = FontFamily(Font(R.font.poppins)),
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = calcSp(24)
-                )
-
-                Spacer(modifier = Modifier.height(calcDp(34.76)))
-
-                Column(verticalArrangement = Arrangement.SpaceBetween) {
-                    CreateTextWithIcon(
-                        text = "SmartRecruiters candidate portal is now Smartr.",
-                        id = R.drawable.resource_switch,
-                        contentDescriptionOfIcon = ""
-                    )
-                    Spacer(modifier = Modifier.height(calcDp(34.46)))
-
-                    CreateTextWithIcon(
-                        text = "Enter the same login info used for your SmartrProfile",
-                        id = R.drawable.log_in,
-                        contentDescriptionOfIcon = ""
-                    )
-                    Spacer(modifier = Modifier.height(calcDp(34.46)))
-
-                    CreateTextWithIcon(
-                        text = "If login details were saved, you may need to re-save.",
-                        id = R.drawable.refresh,
-                        contentDescriptionOfIcon = ""
-                    )
-
-                }
-                Spacer(modifier = Modifier.height(calcDp(48.43)))
-
-                Text(
-                    text = "Why Smartr? Read here",
-                    fontFamily = FontFamily(Font(R.font.poppins)),
-                    fontSize = calcSp(10),
-                    fontWeight = FontWeight.SemiBold,
-                    color = Color(0xFFF4F4F4),
+                    text = "GET STARTED",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center
                 )
-                Spacer(modifier = Modifier.height(calcDp(8.99)))
-
-                Button(
-                    onClick = {},
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFFBFDBD1),
-                        contentColor = Color(0xCC135B46)
-                    ),
-                    shape = RoundedCornerShape(30.dp),
-                    modifier = Modifier
-                        .width(calcDp(inDpI = 274))
-                        .height(calcDp(inDpI = 42))
-                ) {
-                    Text(
-                        text = "GET STARTED",
-                        fontSize = calcSp(15),
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center
-                    )
-                }
             }
         }
     }
 }
-
-val percentageToMultiply = 1.15
-
-@Composable
-fun calcDp(inDp: Double = 0.0, inDpI: Int = 0): Dp {
-    if (inDp == 0.0)return (inDpI * percentageToMultiply).dp
-    return (inDp * percentageToMultiply).dp
-}
-
-@Composable
-fun calcSp(spV: Int = 0): TextUnit {
-    return (spV * percentageToMultiply).sp
-}
-
 
 @Composable
 fun CreateTextWithIcon(
@@ -165,23 +160,20 @@ fun CreateTextWithIcon(
             painter = painterResource(id),
             contentDescription = contentDescriptionOfIcon,
             modifier = Modifier
-                .width(calcDp(23.73))
-                .height(calcDp(26.02)),
+                .width((23.73.dp))
+                .height((26.02.dp)),
             tint = Color(0xFFF4F4F4)
         )
-
         Text(
             text = text,
             color = Color(0xFFF4F4F4),
-            fontFamily = FontFamily(Font(R.font.poppins)),
-            fontSize = calcSp(12),
+            fontFamily = FontFamily(Font(R.font.poppins6)),
+            fontSize = 12.sp,
             fontWeight = FontWeight.SemiBold,
             modifier = Modifier
-                .height(calcDp(48.5))
-                .width(calcDp(134.51))
-                .padding(start = calcDp(16.33))
+                .height((48.5).dp)
+                .width((134.51.dp))
+                .padding(start = (16.33.dp))
         )
-
     }
-
 }
--- app/src/main/java/com/example/assignment01/SecondPage.kt
+++ app/src/main/java/com/example/assignment01/SecondPage.kt
@@ -3,10 +3,234 @@ package com.example.assignment01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class SecondPage: ComponentActivity() {
+class SecondPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{}
+        setContent {
+            Second()
+        }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun Second() {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(30.dp))
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .background(Color(0x2E12AA73))
+                .fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(60.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .width(380.dp)
+                    .height(45.dp)
+                    .offset(x = 9.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ellipse),
+                    contentDescription = "",
+                    Modifier
+                        .width(45.dp)
+                        .height(45.dp)
+                        .clip(CircleShape)
+                )
+
+                Image(
+                    painter = painterResource(R.drawable.notification),
+                    contentDescription = "",
+                    Modifier
+                        .width(24.dp)
+                        .height(24.dp)
+                )
+            }
+
+
+            CreateBox("Junior UX Designer", Type.APPLIED)
+
+        }
+    }
+}
+
+@Composable
+fun CreateBox(titleJob: String, type: Type) {
+    Box(
+        modifier = Modifier
+            .width(380.dp)
+            .height(103.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .background(Color.White)
+    ) {
+        Box(modifier = Modifier.padding(start = 16.dp)) {
+            Column(modifier = Modifier.offset(y = 17.dp)) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Row {
+                        Image(
+                            painter = painterResource(R.drawable.rectangle),
+                            contentDescription = "",
+                            modifier = Modifier.size(35.dp)
+                        )
+                        Column(modifier = Modifier.offset(12.dp)) {
+                            Text(
+                                text = titleJob,
+                                fontSize = 14.sp,
+                                fontFamily =  FontFamily(Font(R.font.poppins6)),
+                                color = Color(0xFF0D0D0D)
+                            )
+                            Text(
+                                text = "Canva",
+                                fontFamily = FontFamily(Font(R.font.poppins5)),
+                                color = Color(0xFF434545),
+                                fontSize = 12.sp,
+                                style = TextStyle(
+                                    letterSpacing = 0.25.sp,
+                                    lineHeight = (12 * 1.3f).sp
+                                )
+                            )
+                        }
+                    }
+                    doMark(type)
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth().offset(y= 11.dp).padding(end = 11.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .width(78.dp)
+                            .height(24.dp)
+                            .clip(RoundedCornerShape(30.dp))
+                            .background(Color(0x2E12AA73)),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            "Paystack",
+                            fontFamily = FontFamily(Font(R.font.poppins5)),
+                            fontSize = 12.sp,
+                            style = TextStyle(
+                                letterSpacing = 0.25.sp,
+                                lineHeight = (12 * 1.3f).sp, color = Color(0xFF53595F)
+                            )
+                        )
+                    }
+
+                    Text(
+                        "\$40-60k/yearly",
+                        fontFamily = FontFamily(Font(R.font.poppins5)),
+                        style = TextStyle(
+                            fontSize = 12.sp,
+                            lineHeight = (12 * 1.3f).sp,
+                            letterSpacing = 0.25.sp,
+                            color = Color(0xFF53595F)
+                        )
+
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun doMark(type: Type) {
+    if (type == Type.NONE) return
+    var text = ""
+    var icon = 0
+    var width = 0
+    var color: Color? = null;
+    if (type == Type.APPLIED) {
+        text = "Applied";
+        icon = R.drawable.done_circular
+        color = Color(0xFF07864B)
+        width = 69
+    } else {
+        text = "Expires Soon"
+        icon = R.drawable.info
+        color = Color(0xFFDAA400)
+        width = 93
+    }
+    Box(
+        modifier = Modifier
+            .width(width.dp)
+            .height(20.dp)
+            .background(color),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Icon(
+                painter = painterResource(icon), contentDescription = null, tint = Color.White
+            )
+            Text(
+                text = text, fontSize = 10.sp,
+                fontFamily = FontFamily(Font(R.font.poppins6)),
+                color = Color.White
+            )
+        }
+    }
+
+}
+
+enum class Type {
+    APPLIED, EXPRESS, NONE;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
